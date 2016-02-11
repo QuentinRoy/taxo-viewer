cfaac800ddafc9eb117c15cb127740b8dfb12e55
Quentin Roy <quentin@quentinroy.fr>
2016-02-11 11:35:23 +0800

    Remove lodash dependency

--- src/templates/initials.js
+++ src/templates/initials.js
@@ -1,7 +1,5 @@
-import isArray from "lodash-es/isArray";
-
 module.exports = (names) => (
-    isArray(names) ? names : names.split(" ")
+    Array.isArray(names) ? names : names.split(" ")
 ).map(
     (name) => `${name[0]}.`
 )[0];
